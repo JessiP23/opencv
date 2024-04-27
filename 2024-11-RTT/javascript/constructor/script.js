123f8dc24a35065bbcff8cd335bd3dc5e555f1c2
Jessi Pavia <jessi316866@gmail.com>
2024-04-27 17:10:18 -0400

    constructor added

--- 2024-11-RTT/javascript/constructor/script.js
+++ 2024-11-RTT/javascript/constructor/script.js
@@ -16,3 +16,33 @@ const adventurer = {
         console.log(`${this.name} rolled a ${result}`);
     }
 }
+
+class Character {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+}
+
+const robin = new Character('Robin');
+robin.inventory = ['sword', 'potion', 'artifact'];
+robin.companion = new Character('Leo');
+robin.companion.type = 'Cat';
+robin.companion.companion = new Character('Frank');
+robin.companion.companion.type = 'Flea';
+robin.companion.companion.inventory = ['small', 'hat', 'sunglasses'];
+
+//child class inherits all properties of its parents, so in this case we dont accont name, health, inventory, or roll.
+
+class adventure extends Character {
+    constructor (name, role) {
+        super(name);
+        this.role = role;
+        this.inventory.push('bedroll', '50 gold coins');
+    }
+    scout() {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
+    }
+}
